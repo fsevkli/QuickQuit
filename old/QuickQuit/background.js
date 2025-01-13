950fef304c00c0db5fcb981b7d0eaacce71a95ee
fsevkli <yusufsevkli@gmail.com>
2025-01-13 13:14:40 +0000

    finito

--- old/QuickQuit/background.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onMessageExternal.addListener(function(request, sender, sendResponse) {
-    if (request && request.message === "version") {
-        sendResponse({ version: 1.0 });
-    }
-});
