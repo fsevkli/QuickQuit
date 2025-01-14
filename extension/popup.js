c1d300dee9fb34521a2b172d9dc12b7a3ebefd70
NathanChlysta <chlystnr@miamioh.edu>
2025-01-14 11:55:48 +0000

    Popup changes
    They wont work lol

--- extension/popup.js
+++ extension/popup.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+$(document).ready(function () { // This is the jQuery equivalent of "DOMContentLoaded"
     function getRandomInt(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
@@ -6,9 +6,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const programsTerminated = getRandomInt(3, 10);
     const memorySaved = programsTerminated * getRandomInt(25, 40);
 
-    // Set the values to the DOM elements
-    document.getElementById('programsTerminated').innerText = programsTerminated;
-    document.getElementById('memorySaved').innerText = memorySaved;
+    // Set the values to the DOM elements using jQuery
+    $('#programsTerminated').text(programsTerminated);
+    $('#memorySaved').text(memorySaved);
 
-    // No need for event listener for actionButton if it doesn't exist
+    // If you need to add any event listeners, you can use jQuery's .on() method here
 });
