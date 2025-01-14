10d649f4f271dde77bc9b07d0a77a9c56dde873e
NathanChlysta <chlystnr@miamioh.edu>
2025-01-14 11:58:38 +0000

    Update popup.js
    it still wont work

--- extension/popup.js
+++ extension/popup.js
@@ -1,4 +1,4 @@
-$(document).ready(function () { // This is the jQuery equivalent of "DOMContentLoaded"
+$(document).ready(function() {  // This ensures the DOM is fully loaded
     function getRandomInt(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
@@ -6,9 +6,12 @@ $(document).ready(function () { // This is the jQuery equivalent of "DOMContentL
     const programsTerminated = getRandomInt(3, 10);
     const memorySaved = programsTerminated * getRandomInt(25, 40);
 
-    // Set the values to the DOM elements using jQuery
-    $('#programsTerminated').text(programsTerminated);
-    $('#memorySaved').text(memorySaved);
-
-    // If you need to add any event listeners, you can use jQuery's .on() method here
+    // Ensure the elements exist and are available before accessing them
+    if ($('#programsTerminated').length && $('#memorySaved').length) {
+        // Set the values to the DOM elements using jQuery
+        $('#programsTerminated').text(programsTerminated);
+        $('#memorySaved').text(memorySaved);
+    } else {
+        console.error('Elements not found in the DOM');
+    }
 });
